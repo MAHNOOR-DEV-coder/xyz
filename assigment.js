92d1841063325969ba2caf59a8dc2e0c1c52e48e
Mahnoor Shahzad <its.mahnoor941@gmail.com>
2026-03-10 11:11:54 +0500

    Add files via upload

---
+++ assigment.js
@@ -0,0 +1,40 @@
+// ASSIGMENT       
+
+studentName 
+ studentAge 
+ homeCity
+  schoolName 
+favoriteSubject 
+
+studentName2
+ studentAge 
+ homeCity
+  schoolName 
+favoriteSubject 
+
+studentName3
+ studentAge 
+ homeCity
+  schoolName 
+favoriteSubject 
+
+
+// Make sure ya  console par or alert par alag alag show ho .
+
+
+// and find ligall or iligall 
+
+
+1.var age // find ya variable ligall hai ya iligall 
+
+2.var 2city// find 
+
+3.var student_name //find
+
+
+
+//illegal variable names ko correct karein.
+
+var 1student = "Ali";
+var user name = "Ahmed";
+var class = "10";
